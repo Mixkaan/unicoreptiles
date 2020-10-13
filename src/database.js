03adee17a1f69aa82d94f0d24c3e1f56cbf24244
leonardo basteri <unicreptiles@gmail.com>
2020-10-13 01:53:50 -0500

    ola

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 //-----BASE-DE-DATOS-PRODUCCION-----//
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://unicoreptiles:Elizabeth123@cluster0.okzuz.mongodb.net/Cluster0?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb+srv://unicoreptiles:Elizabeth123@cluster0.okzuz.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// });
 
 
 //-----BASE-DE-DATOS-DESARROLLO-----//
@@ -15,3 +15,13 @@ mongoose.connect('mongodb+srv://unicoreptiles:Elizabeth123@cluster0.okzuz.mongod
 
 //   .then(db => console.log('Base de datos conectada'))
 //   .catch(err => console.log(err));
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://unicoreptiles:Elizabeth123@cluster0.okzuz.mongodb.net/Cluster0?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
