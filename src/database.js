1d89b03115089dea99f8d5ca7383f7c943ad2cbf
leonardo basteri <unicreptiles@gmail.com>
2020-10-13 01:50:14 -0500

    ola

--- src/database.js
+++ src/database.js
@@ -1,18 +1,17 @@
 //-----BASE-DE-DATOS-PRODUCCION-----//
 const mongoose = require('mongoose');
-
-// mongoose.connect('mongodb+srv://unicoreptiles:Elizabeth123@cluster0.okzuz.mongodb.net/Cluster0?retryWrites=true&w=majority', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
+mongoose.connect('mongodb+srv://unicoreptiles:Elizabeth123@cluster0.okzuz.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 
 //-----BASE-DE-DATOS-DESARROLLO-----//
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/unico', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/unico', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
 
-  .then(db => console.log('Base de datos conectada'))
-  .catch(err => console.log(err));
+//   .then(db => console.log('Base de datos conectada'))
+//   .catch(err => console.log(err));
