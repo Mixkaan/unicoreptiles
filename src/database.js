91531be934b49ab0b8f79fd3025b9e8dea1a6da0
leonardo basteri <unicreptiles@gmail.com>
2020-10-13 00:09:47 -0500

    ggg

--- src/database.js
+++ src/database.js
@@ -1,9 +1,9 @@
 //-----BASE-DE-DATOS-PRODUCCION-----//
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb+srv://unico-reptiles:Elizabeth123@cluster0.qb2n1.mongodb.net/Cluster0?retryWrites=true&w=majority', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://unico-reptiles:Elizabeth123@cluster0.qb2n1.mongodb.net/Cluster0?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 
 //-----BASE-DE-DATOS-DESARROLLO-----//
